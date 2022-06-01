9689707922b8c6d091b0e69c640e437aebe71062
Alejandro de Frutos <adefrutos-e@sice-outsourcing.com>
2022-06-01 18:15:04 +0200

    Fix post tag

--- src/app/redux/api/index.ts
+++ src/app/redux/api/index.ts
@@ -2,11 +2,13 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
 import { baseQueryWithReauth } from './../utils'
 
+export const POST_TAG = 'Post'
+
 // Define a service using a base URL and expected endpoints
 const api = createApi({
   reducerPath: 'api',
   baseQuery: baseQueryWithReauth,
-  tagTypes: ['POST'],
+  tagTypes: [POST_TAG],
   endpoints: () => ({})
 })
 
