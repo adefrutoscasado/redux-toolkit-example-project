a0698b39eec706774f432d6639e5819314c2b95e
Alejandro de Frutos <adefrutoscasado@gmail.com>
2022-06-04 11:51:53 +0200

    Move user reducer hook

---
+++ src/features/login/hooks.ts
@@ -0,0 +1,41 @@
+
+import { logoutAction, loginAsyncThunk, selectSession } from './sessionSlice'
+import { useAppDispatch, useAppSelector } from './../../app/hooks'
+
+export const useUserReducer = () => {
+  const dispatch = useAppDispatch()
+  const {
+    error: loginError,
+    isFetching: isLoggingIn,
+    data: session,
+  } = useAppSelector(selectSession)
+
+  const access_token = session?.access_token
+  const refresh_token = session?.refresh_token
+  const user = session?.user
+
+  const isLoggedIn = !!session
+
+  const logout = () => dispatch(logoutAction())
+
+  const login = ({ username, password }) => dispatch(loginAsyncThunk({ username, password }))
+
+  const isAdmin = user?.role === 'admin'
+  const isUser = user?.role === 'user'
+
+  const defaultRoute = '/'
+
+  return {
+    login,
+    logout,
+    access_token,
+    refresh_token,
+    user,
+    isAdmin,
+    isUser,
+    isLoggedIn,
+    defaultRoute,
+    loginError,
+    isLoggingIn,
+  }
+}
