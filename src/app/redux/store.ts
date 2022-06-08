e6ab5c8cdb3269c884913836e507a1ed0eb18c27
Alejandro de Frutos <adefrutoscasado@gmail.com>
2022-06-08 16:53:34 +0200

    Fix session not being persisted well

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -4,36 +4,6 @@ import counterReducer from '../../features/counter/counterSlice'
 import sessionReducer from '../../features/login/sessionSlice'
 import todoReducer from '../../features/todo/todoSlice'
 import api from './api/index'
-import { StateFromReducersMapObject } from "@reduxjs/toolkit"
-
-
-const PERSISTED_STATE = 'persisted-state'
-
-const loadState = () => {
-  try {
-    const serializedState = localStorage.getItem(PERSISTED_STATE)
-    return JSON.parse(serializedState || '{}')
-  } catch (error) {
-    return undefined
-  }
-}
-
-const saveState = (state: any) => {
-  // REVIEW: Is there a better way to manage persistent session?
-  try {
-    if (state.session.data) {
-      const serializedState = JSON.stringify({ session: state.session })
-      localStorage.setItem(PERSISTED_STATE, serializedState)
-    }
-    if (state.session.isFetching || state.session.error || !state.session) {
-      localStorage.removeItem(PERSISTED_STATE)
-    }
-  } catch (error) {
-    // Ignore write errors
-  }
-}
-
-const persistedState = loadState()
 
 const reducer = {
   counter: counterReducer,
@@ -42,20 +12,13 @@ const reducer = {
   [api.reducerPath]: api.reducer,
 }
 
-type reducerType = StateFromReducersMapObject<typeof reducer>
-
 export const store = configureStore({
-  preloadedState: persistedState as reducerType,
   reducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware)
 })
 
 setupListeners(store.dispatch)
-
-store.subscribe(() => {
-  saveState(store.getState())
-})
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
